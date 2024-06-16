13cbedf49f0321cf21cbab1f9f792f3a1fc967d0
Apurva Upadhye <apurvaupadhye25@gmail.com>
2024-06-16 16:46:34 +0530

    second commit

--- apps/student/components/form/SignInForm.tsx
+++ apps/student/components/form/SignInForm.tsx
@@ -1,7 +1,73 @@
-import React from "react";
+"use client";
+
+import { Form } from "../../@/components/ui/form";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  FormField,
+  FormControl,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../../@/components/ui/form";
+import { Button } from "../../@/components/ui/button";
+import { Input } from "../../@/components/ui/input";
+
+const FormSchema = z.object({
+  email: z.string(),
+  password: z.string(),
+});
 
 const SignInForm = () => {
-  return <div>SignInFrom</div>;
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    // defaultValues: {
+    //   email: "",
+    // },
+  });
+
+  const onSubmit = () => {
+    console.log("form submitted");
+  };
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
+        <div className="space-y-2 space-x-2">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="abc@gmail.com" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter your password" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <Button className="w-full mt-6" type="submit">
+          Sign in
+        </Button>
+      </form>
+    </Form>
+  );
 };
 
 export default SignInForm;
